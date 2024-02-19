b86fd1e5ada540604c011906ee298aca930ad233
hanwool.oh <hanwool.oh@sebasi.co.kr>
2024-02-20 07:44:48 +0900

    [FEAT] first init

---
+++ TCACoordinatorExampleModified/TCACoordinatorExampleModifiedApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCACoordinatorExampleModifiedApp.swift
+//  TCACoordinatorExampleModified
+//
+//  Created by hanwool oh on 2/20/24.
+//
+
+import SwiftUI
+
+@main
+struct TCACoordinatorExampleModifiedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
